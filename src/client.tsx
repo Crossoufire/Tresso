31bde398a477bbe7bcfc1bba2351596519910bed
Crossoufire <47953962+Crossoufire@users.noreply.github.com>
2025-11-19 20:11:17 +0100

    i guess no mobile dnd then

--- src/client.tsx
+++ src/client.tsx
@@ -1,20 +1,5 @@
 import {hydrateRoot} from "react-dom/client";
-import * as MobileDragDrop from "mobile-drag-drop";
 import {StartClient} from "@tanstack/react-start/client";
-import {scrollBehaviourDragImageTranslateOverride} from "mobile-drag-drop/scroll-behaviour";
-
-
-if (typeof window !== "undefined" && !(window as any).__mobile_drag_drop_polyfilled) {
-    (window as any).__mobile_drag_drop_polyfilled = true;
-
-    MobileDragDrop.polyfill({
-        holdToDrag: 300,
-        dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride,
-    });
-
-    window.addEventListener("touchmove", () => {
-    }, { passive: false });
-}
 
 
 hydrateRoot(document, <StartClient/>);
