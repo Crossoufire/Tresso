fbb9784c8002f65fb96c25555472791f18b63e07
Crossoufire <47953962+Crossoufire@users.noreply.github.com>
2025-11-19 20:05:41 +0100

    mobile dnd ??

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,7 +4,6 @@ import React, {lazy} from "react";
 import appCss from "~/styles.css?url";
 import {QueryClient} from "@tanstack/react-query";
 import {authOptions} from "~/lib/client/react-query/query-options";
-import {useMobileDragDrop} from "~/lib/client/hooks/use-mobile-dnd";
 import {createRootRouteWithContext, HeadContent, Outlet, Scripts} from "@tanstack/react-router";
 
 
@@ -37,8 +36,6 @@ function RootComponent() {
 
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-    useMobileDragDrop();
-
     return (
         <html suppressHydrationWarning>
         <head>
